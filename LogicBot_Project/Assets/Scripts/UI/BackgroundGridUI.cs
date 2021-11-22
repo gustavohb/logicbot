84840c94ec3ebb1dd9d2594378c4914dada5626a
Gustavo H. Barrionuevo <gustavohb@users.noreply.github.com>
2021-11-22 13:54:59 -0300

    Improve the animated background grid

--- LogicBot_Project/Assets/Scripts/UI/BackgroundGridUI.cs
+++ LogicBot_Project/Assets/Scripts/UI/BackgroundGridUI.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using DG.Tweening;
 using UnityEngine;
@@ -7,22 +8,48 @@ using ScriptableObjectArchitecture;
 [RequireComponent(typeof(GridLayoutGroup))]
 public class BackgroundGridUI : MonoBehaviour
 {
+    [Header("Settings")]
     [SerializeField] private Color _startColor = Color.clear;
     [SerializeField] private Color _endColor = Color.white;
-
-    [SerializeField] private float _delayFactor = 0.1f;
     
     [SerializeField] private int _gridCols = 16;
     [SerializeField] private int _gridRows = 12;
+    [SerializeField] private int _cellSpacing = 7;
 
+    [SerializeField] private float _delayFactor = 0.1f;
+    
+    [SerializeField] private float _animateTileDuration = 0.5f;
+    [SerializeField] private float _animateSingleTileTime = 1f;
+
+    [SerializeField] private int _minBlinkTileCol = 4;
+    [SerializeField] private int _minBlinkTileRow = 4;
+    
+    [SerializeField] private int _maxBlinkTileCol = 10;
+    [SerializeField] private int _maxBlinkTileRow = 10;
+    
+    [Header("References")]
+    
+    [SerializeField] private CanvasScaler _canvasScaler;
     [SerializeField] private Image _tileImagePrefab;
     
+    [Header("Events")]
     [SerializeField] private GameEvent _levelCompletedGameEvent;
-    
+
     private List<Image> _gridTileImageList = new List<Image>();
+    private GridLayoutGroup _gridLayoutGroup;
+    private Vector2 _referenceScreenResolution;
+    private float _showSingleTileTimer; 
+    private float _showSingleTileDuration;
     
     private void Awake()
     {
+        _referenceScreenResolution = _canvasScaler.referenceResolution;
+        
+        _gridLayoutGroup = GetComponent<GridLayoutGroup>();
+        _gridLayoutGroup.spacing = new Vector2(_cellSpacing, _cellSpacing);
+        _gridLayoutGroup.padding = new RectOffset(_cellSpacing, _cellSpacing, _cellSpacing, _cellSpacing);
+        _gridLayoutGroup.cellSize = CalculateCellSize();
+
         for (int i = 0; i < _gridRows; i++)
         {
             for (int j = 0; j < _gridCols; j++)
@@ -34,149 +61,165 @@ public class BackgroundGridUI : MonoBehaviour
         }
         _levelCompletedGameEvent.AddListener(ShowBackgroundGrid);
     }
+
+    private void Start()
+    {
+        _showSingleTileTimer = _animateSingleTileTime;
+    }
+
+    private void Update()
+    {
+        BlinkTiles();
+    }
+
+    private void BlinkTiles()
+    {
+        if (_showSingleTileTimer > 0)
+        {
+            _showSingleTileTimer -= Time.deltaTime;
+            if (_showSingleTileTimer <= 0)
+            {
+                int col = UnityEngine.Random.Range(_minBlinkTileCol, _maxBlinkTileCol);
+                int row = UnityEngine.Random.Range(_minBlinkTileRow, _maxBlinkTileRow);
+                
+                
+                BlinkTiles(0.01f, col, row);
+                
+                col = UnityEngine.Random.Range(_minBlinkTileCol, _maxBlinkTileCol);
+                row = UnityEngine.Random.Range(_minBlinkTileRow, _maxBlinkTileRow);
+                
+                BlinkTiles(0.15f, col, row);
+            }
+        }
+    }
+
+    private void BlinkTiles(float delay, int col, int row)
+    {
+        BlinkSingleTile(0, col, row);
+        BlinkRandomNeighborTiles(1f, col, row);
+    }
+
+    private void BlinkRandomNeighborTiles(float delay, int col, int row)
+    {
+        int rand = UnityEngine.Random.Range(0, 5);
+        switch (rand)
+        {
+            case 0:
+                return;
+            case 1:
+                if (row + 1 < _gridRows)
+                {
+                    BlinkSingleTile(delay, col, row + 1);
+                }
+                break;
+            case 2:
+                if (col - 1 >= 0)
+                {
+                    BlinkSingleTile(delay + 0.1f, col - 1, row);
+                    if (col - 2 >= 0)
+                    {
+                        BlinkSingleTile(delay + 0.15f, col - 2, row);
+                    }
+                }
+                break;
+            case 3:
+                if (col + 1 < _gridCols)
+                {
+                    BlinkSingleTile(delay + 0.1f, col + 1, row);
+                }
+                break;
+            case 4:
+                if (col + 1 < _gridCols)
+                {
+                    BlinkSingleTile(delay + 0.1f, col + 1, row);
+                    if (row + 1 < _gridRows)
+                    {
+                        BlinkSingleTile(delay + 0.15f, col + 1, row + 1);
+                    }
+                }
+                break;
+        }
+    }
+
+    private void BlinkSingleTile(float delay, int col, int row)
+    {
+        Image currentTileImage = _gridTileImageList[row * _gridCols + col];
+        AnimateTile(delay, _animateTileDuration, currentTileImage, _endColor, () =>
+        {
+            AnimateTile(0.01f, _animateTileDuration, currentTileImage, _startColor, () =>
+            {
+                _showSingleTileTimer = _animateSingleTileTime;
+            });
+        });
+    }
     
-    [ContextMenu("Animate Background Grid")]
+    private Vector2 CalculateCellSize()
+    {
+        float screenWidth = _referenceScreenResolution.x;
+        float screenHeight = _referenceScreenResolution.y;
+        
+        float horizontalSpacing = (_gridCols + 1) * _cellSpacing;
+        float verticalSpacing = (_gridRows + 1) * _cellSpacing;
+
+        float availableHorizontalTileSpace = screenWidth - horizontalSpacing;
+        float availableVerticalTileSpace = screenHeight - verticalSpacing;
+
+        Vector2 cellSize =
+            new Vector2(availableHorizontalTileSpace / _gridCols, availableVerticalTileSpace / _gridRows);
+
+        return cellSize;
+    }
+    
     public void ShowBackgroundGrid()
     {
-        int n = _gridCols;
-        int m = _gridRows;
-        
-        int row = 0, col = 0;
-        bool up = true;
-        
-        float delayFactor = 0.01f;
+        int x = UnityEngine.Random.Range(0, _gridCols);
+        int y = UnityEngine.Random.Range(0, _gridRows);
 
-        while (row < m && col < n)
-        {
-            Image currentTileImage = null;
-            if (up)
+        int maxDist = 0;
+        
+        for (int col = 0; col < _gridCols; col++) {
+            for (int row = 0; row < _gridRows; row++)
             {
-                while (row > 0 && col < n - 1)
+                int dist = Mathf.Abs(x - col) + Mathf.Abs(y - row); // calculate distance
+                if (maxDist < dist)
                 {
-                    currentTileImage = _gridTileImageList[row * n + col];
-                    if (currentTileImage != null)
-                    {
-                        AnimateTile((row + col) * _delayFactor, currentTileImage, _endColor);
-                    }
-                    row--;
-                    col++;
+                    maxDist = dist;
                 }
-                currentTileImage = _gridTileImageList[row * n + col];
+                Image currentTileImage = _gridTileImageList[row * _gridCols + col];
                 if (currentTileImage != null)
                 {
-                    AnimateTile((row + col) * _delayFactor, currentTileImage, _endColor);
-                }
-                if (col == n - 1)
-                {
-                    row++;
-                }
-                else
-                {
-                    col++;
+                    AnimateTile((dist) * _delayFactor, _animateTileDuration, currentTileImage, _endColor);
                 }
             }
-            else
-            {
-                while (col > 0 && row < m - 1)
-                {
-                    currentTileImage = _gridTileImageList[row * n + col];
-                    if (currentTileImage != null)
-                    {
-                        AnimateTile((row + col) * _delayFactor, currentTileImage, _endColor);
-                    }
-                    row++;
-                    col--;
-                }
-                currentTileImage = _gridTileImageList[row * n + col];
-                if (currentTileImage != null)
-                {
-                    AnimateTile((row + col) * _delayFactor, currentTileImage, _endColor);
-                }
-                if (row == m - 1)
-                {
-                    col++;
-                }
-                else
-                {
-                    row++;
-                }
-            }
-            up = !up;
         }
-        this.Wait((n + m) / 1.5f * _delayFactor, HideBackgroundGrid);
+        
+        this.Wait((float)maxDist / 1.4f * _delayFactor, () => HideBackgroundGrid(x, y));
     }
-    
-    public void HideBackgroundGrid()
-    {
-        int n = _gridCols;
-        int m = _gridRows;
-        
-        int row = 0, col = 0;
-        bool up = true;
-        
-        float delayFactor = 0.01f;
 
-        while (row < m && col < n)
+    public void HideBackgroundGrid(int x, int y)
+    {
+        Image currentTileImage;
+        
+        for (int col = 0; col < _gridCols; col++)
         {
-            Image currentTileImage = null;
-            if (up)
+            for (int row = 0; row < _gridRows; row++)
             {
-                while (row > 0 && col < n - 1)
-                {
-                    currentTileImage = _gridTileImageList[row * n + col];
-                    if (currentTileImage != null)
-                    {
-                        AnimateTile((row + col) * _delayFactor, currentTileImage, _startColor);
-                    }
-                    row--;
-                    col++;
-                }
-                currentTileImage = _gridTileImageList[row * n + col];
+                int dist = Mathf.Abs(x - col) + Mathf.Abs(y - row); // calculate distance
+
+                currentTileImage = _gridTileImageList[row * _gridCols + col];
                 if (currentTileImage != null)
                 {
-                    AnimateTile((row + col) * _delayFactor, currentTileImage, _startColor);
-                }
-                if (col == n - 1)
-                {
-                    row++;
-                }
-                else
-                {
-                    col++;
+                    AnimateTile((dist) * _delayFactor, _animateTileDuration, currentTileImage, _startColor);
                 }
             }
-            else
-            {
-                while (col > 0 && row < m - 1)
-                {
-                    currentTileImage = _gridTileImageList[row * n + col];
-                    if (currentTileImage != null)
-                    {
-                        AnimateTile((row + col) * _delayFactor, currentTileImage, _startColor);
-                    }
-                    row++;
-                    col--;
-                }
-                currentTileImage = _gridTileImageList[row * n + col];
-                if (currentTileImage != null)
-                {
-                    AnimateTile((row + col) * _delayFactor, currentTileImage, _startColor);
-                }
-                if (row == m - 1)
-                {
-                    col++;
-                }
-                else
-                {
-                    row++;
-                }
-            }
-            up = !up;
         }
     }
-    
-    private void AnimateTile(float delay, Image tileImage, Color endColor)
+
+    private void AnimateTile(float delay, float duration, Image tileImage, Color endColor, Action onComplete = null)
     {
-        tileImage.DOColor(endColor, 0.5f).SetDelay(delay);
+        tileImage.DOColor(endColor, duration).SetDelay(delay).OnComplete(() =>
+        {
+            onComplete?.Invoke();
+        });
     }
 }
