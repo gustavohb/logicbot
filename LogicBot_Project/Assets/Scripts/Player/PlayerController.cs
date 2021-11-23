c65c9a7653c1c16a54d2b55397441df341b4bb6d
Gustavo H. Barrionuevo <gustavohb@users.noreply.github.com>
2021-11-23 09:32:09 -0300

    Remove commented out code

--- LogicBot_Project/Assets/Scripts/Player/PlayerController.cs
+++ LogicBot_Project/Assets/Scripts/Player/PlayerController.cs
@@ -203,7 +203,6 @@ public class PlayerController : MonoBehaviour
             {
                 _currentHeight = tileHeight;
                 _currentCallback?.Invoke();
-                //_currentCallback = null;
             });
         }
         else
